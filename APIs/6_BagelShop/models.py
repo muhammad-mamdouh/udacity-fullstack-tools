0d37e6e1d98bebbde4a4bd6cb7ee6504f5255778
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-11 22:37:51 +0200

    feature: Add token authentication feature to the user model class

--- APIs/6_BagelShop/models.py
+++ APIs/6_BagelShop/models.py
@@ -2,8 +2,12 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
+import random, string
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+                            BadSignature, SignatureExpired)
 
 Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(32))
 
 
 class User(Base):
@@ -17,6 +21,24 @@ class User(Base):
 
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # Valid token, but expired
+            return None
+        except BadSignature:
+            # Invalid token
+            return None
+        user_id = data['id']
+        return user_id
 
 
 class Bagel(Base):
