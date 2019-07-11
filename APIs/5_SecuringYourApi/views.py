785304179e0a34a947874a4f2377a1b09ec40c45
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-11 17:05:13 +0200

    feature: Protect specified resource by password

--- APIs/5_SecuringYourApi/views.py
+++ APIs/5_SecuringYourApi/views.py
@@ -1,15 +1,28 @@
-from flask import Flask, jsonify, request, url_for, abort
+from flask import Flask, jsonify, request, url_for, abort, g
+from flask_httpauth import HTTPBasicAuth
 from models import Base, User
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 
-engine = create_engine('sqlite:///users.db')
+engine = create_engine('sqlite:///users.db',
+                       connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(username, password):
+    user = session.query(User).filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        return False
+    else:
+        g.user = user
+        return True
 
 
 @app.route('/api/users', methods=['POST'])
@@ -21,12 +34,13 @@ def new_user():
     if username is None or password is None:
         abort(400)    # Missing arguments
     elif session.query(User).filter_by(username=username).first() is not None:
-        return jsonify({'username': username}), 201     # Already existing user
+        print("existing user")
+        return jsonify({'message': 'user already exists'}), 200
+
     user = User(username=username)
     user.hash_password(password)
     session.add(user)
     session.commit()
-
     return jsonify({ 'username': user.username }), 201,\
            {'Location': url_for('get_user', id = user.id, _external = True)}
 
@@ -37,6 +51,12 @@ def get_user(user_id):
     if not user:
         abort(400)
     return jsonify({'username': user.username})
+
+
+@app.route('/api/protected_resource')
+@auth.login_required
+def get_resource():
+    return jsonify({ 'data': f'Hello, {g.user.username}!' })
 
 
 if __name__ == '__main__':
