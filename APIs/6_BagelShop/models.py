4e4ca5ff7011a93d17d16c5fdfeec934747c237c
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-11 17:30:53 +0200

    feature: Add user model class

--- APIs/6_BagelShop/models.py
+++ APIs/6_BagelShop/models.py
@@ -1,10 +1,22 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32))
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.hash(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
 
 class Bagel(Base):
